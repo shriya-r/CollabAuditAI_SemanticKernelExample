e5f57158ca9c639b0a1a8d622630c79c2525e366
Shriya Rajesh <srajesh@usc.edu>
2025-06-05 15:23:05 -0700

    Create InputFilter.cs

---
+++ Plugins/InputFilter/InputFilter.cs
@@ -0,0 +1,22 @@
+using Microsoft.SemanticKernel;
+using CollabAuditAI.CollabQuiz.PluginFunctions;
+
+namespace CollabAuditAI.CollabQuiz.InputFilter
+{
+    public class InputFilter : Plugin
+    {
+        private Kernel kernel;
+        private KernelPlugin pluginFunctions;
+        public InputFilter(Kernel kernel)
+        {
+            this.kernel = kernel;
+            pluginFunctions = KernelBuilder.KernelBuilder.pluginFunctions;
+        }
+        public async Task<string> CallPluginFunction(string topic) // Call plugin to filter inputted topic
+        {
+            var arguments = new KernelArguments() { ["input"] = topic };
+            var result = await kernel.InvokeAsync(pluginFunctions["InputFilter"], arguments);
+            return result.ToString();
+        }
+    }
+}
