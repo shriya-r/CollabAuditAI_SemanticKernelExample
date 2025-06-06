d4a3c8d120de26450fa7af4028768b71b2e09bd4
Shriya Rajesh <srajesh@usc.edu>
2025-06-06 13:37:43 -0700

    Add files via upload

--- Plugins/AIConnector/AIConnector.cs
+++ Plugins/AIConnector/AIConnector.cs
@@ -1,9 +1,11 @@
 using Microsoft.SemanticKernel;
 using CollabAuditAI.CollabQuiz.PluginFunctions;
+using CollabAuditAI.CollabQuiz.ConnectorInterface;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 namespace CollabAuditAI.CollabQuiz.AIConnector
 {
-    public class AIConnector : Plugin
+    public class AIConnector : Plugin, AIConnectorInterface
     {
         private Kernel kernel;
         private KernelPlugin pluginFunctions;
@@ -17,6 +19,20 @@ namespace CollabAuditAI.CollabQuiz.AIConnector
             var arguments = new KernelArguments() { ["input"] = context };
             var result = await kernel.InvokeAsync(pluginFunctions["AIConnector"], arguments);
             return result.ToString();
+        }
+        public async Task<KernelArguments> ChooseLLM(KernelArguments arguments, string topic) // Choose the LLM based on the event
+        {
+            var model_name = await CallPluginFunction(topic);
+            string modelID = "gpt4o";
+            if (model_name.Contains("gpt-4.1-mini"))
+            {
+                modelID = "gpt4.1";
+            }
+            arguments.ExecutionSettings = new Dictionary<string, PromptExecutionSettings>()
+            {
+                [modelID] = new OpenAIPromptExecutionSettings { ServiceId = modelID }
+            };
+            return arguments;
         }
     }
 }
