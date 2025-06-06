9651a33a9aa45f07a050196695c24d21c4bcb2f2
Shriya Rajesh <srajesh@usc.edu>
2025-06-06 13:39:15 -0700

    Add files via upload

--- Plugins/Validation/Validation.cs
+++ Plugins/Validation/Validation.cs
@@ -12,9 +12,13 @@ namespace CollabAuditAI.CollabQuiz.Validation
             this.kernel = kernel;
             pluginFunctions = KernelBuilder.KernelBuilder.pluginFunctions;
         }
-        public async Task<string> CallPluginFunction(string output) // Formatter plugin to pass the result into LLM for desired formatting
+        public async Task<string> CallPluginFunction(string output) // Validate the quiz accuracy
         {
             var arguments = new KernelArguments() { ["input"] = output };
+
+            var AIplugin = new AIConnector.AIConnector(kernel); // Choose the LLM to validate the quiz
+            arguments = await AIplugin.ChooseLLM(arguments, "I need to validate the accuracy of this quiz: " + output);
+
             var result = await kernel.InvokeAsync(pluginFunctions["Validation"], arguments);
             return result.ToString();
         }
