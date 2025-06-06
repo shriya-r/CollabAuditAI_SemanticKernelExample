345ee3bd0f5aafed93cf47c3013e41efcdcf6158
Shriya Rajesh <srajesh@usc.edu>
2025-06-06 13:38:54 -0700

    Add files via upload

--- Plugins/Soccer/SoccerPlugin.cs
+++ Plugins/Soccer/SoccerPlugin.cs
@@ -8,10 +8,14 @@ namespace CollabAuditAI.CollabQuiz.SoccerPlugin
             : base(kernel)
         { // initialize base class
         }
-        public async Task<string> CallSoccer(KernelPlugin pluginFunctions) // Call the soccer plugin with more specific information
+        public async Task<string> CallSoccer(KernelPlugin pluginFunctions)
         {
             // Call the soccer plugin with more specific information
             var arguments = new KernelArguments() { ["input"] = "top soccer players and important game results" };
+
+            var AIplugin = new AIConnector.AIConnector(kernel);
+            arguments = await AIplugin.ChooseLLM(arguments, "I need to generate a quiz about soccer");
+
             var result = await kernel.InvokeAsync(pluginFunctions["Soccer"], arguments);
             return result.ToString();
         }
