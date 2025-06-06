86266e5df5867a47974100a90c510183496e4cbd
Shriya Rajesh <srajesh@usc.edu>
2025-06-06 13:38:01 -0700

    Add files via upload

--- Plugins/InputFilter/InputFilter.cs
+++ Plugins/InputFilter/InputFilter.cs
@@ -15,6 +15,8 @@ namespace CollabAuditAI.CollabQuiz.InputFilter
         public async Task<string> CallPluginFunction(string topic) // Call plugin to filter inputted topic
         {
             var arguments = new KernelArguments() { ["input"] = topic };
+            var AIplugin = new AIConnector.AIConnector(kernel);
+            arguments = await AIplugin.ChooseLLM(arguments, "I need to filter this input: " + topic);
             var result = await kernel.InvokeAsync(pluginFunctions["InputFilter"], arguments);
             return result.ToString();
         }
